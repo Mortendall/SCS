620309ff53fe6b19dff6b2334e5666b93238ae5d
Morten Dall <dall@sund.ku.dk>
2020-07-27 13:57:24 +0200

    Update sINGLE CELL TUTORIAL.R

--- sINGLE CELL TUTORIAL.R
+++ sINGLE CELL TUTORIAL.R
@@ -2,5 +2,11 @@ library(tidyverse)
 library(patchwork)
 library(Seurat)
 
-pbmc.data <- Read10X(data.dir = "C:/Users/tvb217/Documents/R/SCS/filtered_gene_bc_matrices/hg19/")
+pbmc.data <- Read10X(data.dir = "C:/Users/tvb217/Documents/R/tmp/SCS/filtered_gene_bc_matrices/hg19/")
 pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1+plot2
